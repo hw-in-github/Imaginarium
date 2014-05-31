be6b8e22ad88858fc032f186f1349996909ae8a0
will hunting <willhunting@wills-Mac.local>
2014-05-31 21:40:52 +0800

    Initial Commit

---
+++ Imaginarium/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by will hunting on 5/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Imaginarium/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Imaginarium
+//
+//  Created by will hunting on 5/31/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
